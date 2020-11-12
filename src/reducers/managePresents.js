767c4d0ebe472dc667c277b3ab15421b83280ff6
sdonohue0918 <sdonohue0918@gmail.com>
2020-11-12 16:53:37 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
 export function managePresents(state, action){
+
+    // action = {
+    //     type: "INCREASE"
+    // }
+
+    if (action.type === "INCREASE") {
+        return {numberOfPresents: state.numberOfPresents + 1}
+    } else {
+        return {numberOfPresents: state.numberOfPresents}
+    }
 }
